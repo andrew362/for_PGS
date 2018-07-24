bbe1e56f47ea2ed1b344953fd9577a035517169a
Andrzej <lichon.andrzej@gmail.com>
2018-07-24 12:49:47 +0200

    mobile layout

--- scripts/emailValuation.js
+++ scripts/emailValuation.js
@@ -1,66 +1,95 @@
 (function () {
-
-
     const form = document.getElementById('contact-form');
     const btn = document.getElementById('send');
     const progressbar = document.getElementById('progressbar');
     let firstPress = true;
     let nameIsValidated = false;
     let emailIsValidated = false;
+
     let nameInput = document.querySelector('input[name="name"]');
     let emailInput = document.querySelector('input[name="email"]');
     
-    form.setAttribute('novalidate', true);
-
     function validateEmail(email) {
         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(String(email).toLowerCase());
     }
 
-    function imie() {
+    function nameCheck() {
         if (nameInput.checkValidity()) {
-            nameIsValidated = true;
             nameInput.className = "valid"
+            nameIsValidated = true;
         } else {
             nameInput.className = "invalid";
         }
     }
 
-    function email() {
-        if (validateEmail(emailInput.value)) {
-            emailIsValidated = true;
-            console.log(emailIsValidated);
+    function emailCheck() {
+        if (validateEmail(emailInput.value)) {    
             emailInput.className = "valid"
+            emailIsValidated = true;
         } else {
             emailInput.className = "invalid";
         }
     }
 
+    function sendForm(field1, field2) {
+        const dataToSend = new FormData();
+        const url = form.getAttribute('action');
+        const method = form.getAttribute('method');
+
+        [field1, field2].forEach(el => dataToSend.append(el.name, el.value));
+        fetch(url, {
+                method: method,
+                body: dataToSend
+            })
+            .then(res => res.json())
+            .then(res => {
+                if (res.status === 'ok') {
+                    M.toast({
+                        html: 'Wiadomość została wysłana!',
+                        classes: 'teal '
+                    });
+                }
+                if (res.status === 'error') {
+                    M.toast({
+                        html: 'Wiadomość nie została wysłana!',
+                        classes: 'red '
+                    });
+                }
+            })
+            .catch(function (err) {
+                console.error('Coś poszło nie tak', err);
+                M.toast({
+                    html: 'Wiadomość nie została wysłana!',
+                    classes: 'red '
+                });
+            })
+            .finally(function () {
+                setTimeout(function () {
+                    progressbar.classList.add('hide');
+                    btn.classList.remove("disabled");
+                }, 1000)
+
+            });
+    }
+
     form.addEventListener('submit', function (e) {
         e.preventDefault();
-        formIsValidated = true;
-        nameInput.addEventListener('input', imie);
-        emailInput.addEventListener('input', email);
-        if (firstPress) {
-            imie();
-            email();
+        nameIsValidated = false;
+        emailIsValidated = false;
+        nameCheck();
+        emailCheck();
+        if (firstPress) { 
+            nameInput.addEventListener('change', nameCheck);
+            emailInput.addEventListener('change', emailCheck);
         }
         firstPress = false;
-        console.log(nameIsValidated, emailIsValidated);
         if (nameIsValidated && emailIsValidated) {
             progressbar.classList.remove('hide');
-            this.submit();
-            btn.classList.toggle("disabled");
+            btn.classList.add("disabled");
+            sendForm(nameInput, emailInput);
         } else {
             return false;
         }
-
     });
-
-
-
-
-
-
-
 })();
